3575d8fb0291a35aaf223654f5ebdc89fbb0373d
pato9310 <pato@PATO.localdomain>
2022-04-27 21:32:41 -0300

    Mariano - User routes and services

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,16 +1,54 @@
-const express = require('express');
+const express = require("express");
 
-const UserService = require('../services/user.service');
+const UserService = require("../services/user.service");
 
 const router = express.Router();
 
 const service = new UserService();
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const users = await service.find();
 
     res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const user = await service.findOne(id);
+
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { body } = req;
+
+    const newUser = await service.create(body);
+
+    res.json(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const { body } = req;
+
+    const updatedUser = await service.update(id, body);
+
+    res.json(updatedUser);
   } catch (error) {
     next(error);
   }
