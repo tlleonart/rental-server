d6408076bc3f9fcf634396c2dc63badc1eb1112e
Nicolas Cohen <nicolascohen24@gmail.com>
2022-04-28 17:50:43 -0300

    New Api Key

--- services/utils.js
+++ services/utils.js
@@ -6,8 +6,8 @@ function hash(string) {
 
 const timeStamp = Math.floor(Date.now() / 1000);
 const url = 'https://api.test.hotelbeds.com/hotel-content-api/1.0/hotels?fields=all&language=ENG&from=1&to=100&useSecondaryLanguage=false';
-const apiKey = '684f98064c4f8f10d22b3ca1a70e743d';
-const secret = '1490581b65';
+const apiKey = 'e58e7642f4275d4b90d210f2af96cb7e';
+const secret = '4af634c7d1';
 const code = apiKey + secret + timeStamp;
 const signature = hash(code);
 
