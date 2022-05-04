fd3bc0e6c59ab26bc69a8e241fdbe6f6ae54b4d9
Nicolas Cohen <nicolascohen24@gmail.com>
2022-05-04 19:11:57 -0300

    Facility Model

---
+++ database/models/facility.model.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const FACILITY_TABLE = 'facilities';
+
+const FacilitySchema = {
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+  },
+  createdAt: {
+    allowNull: false,
+    field: 'created_at',
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+};
+
+class Facility extends Model {
+  static associate() {}
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: FACILITY_TABLE,
+      modelName: 'Facility',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { FACILITY_TABLE, FacilitySchema, Facility };
--- database/models/index.js
+++ database/models/index.js
@@ -3,6 +3,7 @@ const { Hotel, HotelSchema } = require('./hotel.model');
 const { Review, ReviewSchema } = require('./review.model');
 const { Suscriptor, SuscriptorSchema } = require('./suscriptor.model');
 const { Booking, BookingSchema } = require('./booking.model');
+const { Facility, FacilitySchema } = require('./facility.model');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
@@ -10,6 +11,7 @@ function setupModels(sequelize) {
   Review.init(ReviewSchema, Review.config(sequelize));
   Suscriptor.init(SuscriptorSchema, Suscriptor.config(sequelize));
   Booking.init(BookingSchema, Booking.config(sequelize));
+  Facility.init(FacilitySchema, Facility.config(sequelize));
 }
 
 module.exports = setupModels;
