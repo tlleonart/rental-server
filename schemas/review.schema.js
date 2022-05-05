b05cc134527c9b27492a9c19d5e723df6e9f7860
Pato9310 <patito.mdd@gmail.com>
2022-05-05 18:55:49 -0300

    Review Schema (Joi)

--- schemas/review.schema.js
+++ schemas/review.schema.js
@@ -1,24 +1,42 @@
 const Joi = require('joi');
 
 const id = Joi.number();
-const title = Joi.string();
 const description = Joi.string();
 const score = Joi.string().valid('1', '2', '3', '4', '5');
+const priceQualityRatio = Joi.string().valid('1', '2', '3', '4', '5');
+const facilities = Joi.string().valid('1', '2', '3', '4', '5');
+const location = Joi.string().valid('1', '2', '3', '4', '5');
+const cleaning = Joi.string().valid('1', '2', '3', '4', '5');
+const attentionService = Joi.string().valid('1', '2', '3', '4', '5');
+const comfortable = Joi.string().valid('1', '2', '3', '4', '5');
+const stayedOn = Joi.string();
 
 const getReviewSchema = Joi.object({
   id: id.required(),
 });
 
 const createReviewSchema = Joi.object({
-  title: title.required(),
   description: description.required(),
   score: score.required(),
+  priceQualityRatio: priceQualityRatio.required(),
+  facilities: facilities.required(),
+  location: location.required(),
+  cleaning: cleaning.required(),
+  attentionService: attentionService.required(),
+  comfortable: comfortable.required(),
+  stayedOn: stayedOn.required(),
 });
 
 const updateReviewSchema = Joi.object({
-  title,
   description,
   score,
+  priceQualityRatio,
+  facilities,
+  location,
+  cleaning,
+  attentionService,
+  comfortable,
+  stayedOn,
 });
 
 module.exports = { createReviewSchema, updateReviewSchema, getReviewSchema };
