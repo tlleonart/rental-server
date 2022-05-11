98e24701d94c166574d63688aa33037c2e0324d5
LucasNovoa <lucasnovoaok@gmail.com>
2022-05-11 13:41:38 -0300

    async db load

--- services/city.service.js
+++ services/city.service.js
@@ -8,7 +8,7 @@ class CityService {
   constructor() {}
 
   async dbLoadCities() {
-    cities.map((c) => this.create(c));
+    cities.map(async (c) => await this.create(c));
   }
 
   async find() {
