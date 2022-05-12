26d383721518e6924cda117d1f0f7f1fb5ed921e
Nicolas Cohen <nicolascohen24@gmail.com>
2022-05-12 02:02:09 -0300

    Google Auth

--- utils/auth/index.js
+++ utils/auth/index.js
@@ -4,10 +4,6 @@ const LocalStrategy = require('./strategies/local.strategy');
 
 const JwtStrategy = require('./strategies/jwt.strategy');
 
-const GoogleStrategy = require('./strategies/google.strategy');
-
 passport.use(LocalStrategy);
 
 passport.use(JwtStrategy);
-
-passport.use(GoogleStrategy);
