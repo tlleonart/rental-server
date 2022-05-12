61f59650ea1943e0b786fa7e50929308a841a4ae
Lucas <92888202+LucasNovoa@users.noreply.github.com>
2022-05-12 19:12:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://rental-app-client.netlify.app'];
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://rental-app-client.netlify.app', 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=https%3A%2F%2Frental-app-server.herokuapp.com%2Fapi%2Fv1%2Fauth%2Fgoogle%2Fcallback&scope=email%20profile&client_id=673120548612-hukb89n0hg4lhlpr3jkjf1g2l2cb2j3n.apps.googleusercontent.com'];
 
 const corsOptions = {
   origin(origin, callback) {
