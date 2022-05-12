6276c1fbc1e8ca88977e84a758fb96bad4d4c565
LucasNovoa <lucasnovoaok@gmail.com>
2022-05-12 19:27:47 -0300

    deploy v5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const cors = require('cors');
+// const cors = require('cors');
 
 const routerApi = require('./routes');
 
@@ -14,19 +14,19 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://rental-app-client.netlify.app', 'https://rental-app-server.herokuapp.com','https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=https%3A%2F%2Frental-app-server.herokuapp.com%2Fapi%2Fv1%2Fauth%2Fgoogle%2Fcallback&scope=email%20profile&client_id=673120548612-hukb89n0hg4lhlpr3jkjf1g2l2cb2j3n.apps.googleusercontent.com'];
+// const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://rental-app-client.netlify.app', 'https://rental-app-server.herokuapp.com','https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=https%3A%2F%2Frental-app-server.herokuapp.com%2Fapi%2Fv1%2Fauth%2Fgoogle%2Fcallback&scope=email%20profile&client_id=673120548612-hukb89n0hg4lhlpr3jkjf1g2l2cb2j3n.apps.googleusercontent.com'];
 
-const corsOptions = {
-  origin(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not Allowed By CORS'));
-    }
-  },
-};
-
-//app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin(origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not Allowed By CORS'));
+//     }
+//   },
+// };
+//
+// app.use(cors(corsOptions));
 
 require('./utils/auth');
 
