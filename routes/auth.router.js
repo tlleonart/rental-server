d5231a605d41679a62d2b3c3ce54e965ef909816
Pato9310 <patito.mdd@gmail.com>
2022-05-17 11:30:39 -0300

    auth0 config

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,60 +1,40 @@
 const express = require('express');
 
-const passport = require('passport');
+const jwks = require('jwks-rsa');
 
-const session = require('express-session');
+const { expressjwt: jwt } = require('express-jwt');
 
+const { default: axios } = require('axios');
 const AuthService = require('../services/auth.service');
 
 const service = new AuthService();
 
-const UserService = require('../services/user.service');
-
-const userService = new UserService();
-
 const router = express.Router();
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://rental-app-server.us.auth0.com/.well-known/jwks.json',
+  }),
+  audience: 'https://rental-app-server.com',
+  issuer: 'https://rental-app-server.us.auth0.com/',
+  algorithms: ['RS256'],
+});
 
 router.post(
   '/login',
-  passport.authenticate('local', { session: false }),
+  jwtCheck,
   async (req, res, next) => {
     try {
-      const { user } = req;
-
-      res.json(service.signToken(user));
-    } catch (error) {
-      next(error);
-    }
-  },
-);
-
-router.get('/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
-
-router.get(
-  '/google/callback',
-  passport.authenticate('google', { session: true }),
-  async (req, res, next) => {
-    try {
-      // const { user } = req;
-
-      // req.session.user = user.email;
-
-      // res.json(req.session.user);
-      res.redirect(window.location.origin);
-    } catch (error) {
-      next(error);
-    }
-  },
-);
-
-router.get(
-  '/getGoogleUser',
-  async (req, res, next) => {
-    try {
-      const email = req.session.user;
-
-      const user = await userService.findByEmail(email);
-
+      const accessToken = req.headers.authorization.split(' ')[1];
+      const response = await axios.get('https://rental-app-server.us.auth0.com/userinfo', {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const user = response.data;
       res.json(service.signToken(user));
     } catch (error) {
       next(error);
